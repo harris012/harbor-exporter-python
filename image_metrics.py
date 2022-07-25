932b2df5df1f5f179831bc1ab6010097bb419d24
harris012 <harissaeed@protonmail.com>
2022-07-25 21:19:49 +0200

    CVE scanning resulty

--- image_metrics.py
+++ image_metrics.py
@@ -2,16 +2,14 @@
 Functions related to obtaining the timestamp of images.
 """
 
-import urllib.request
+import logging
 import urllib.parse
 import dateutil.parser
-import json
-import logging
 import time
-from cachetools.func import ttl_cache
+import json
 from collections import defaultdict
-DOCKER_REGISTRY_HOST = "WILL NOT TELL YOU" # edit me
-DOCKER_REGISTRY_URL = f"https://{DOCKER_REGISTRY_HOST}/v2"
+from cachetools.func import ttl_cache
+
 IMAGE_LAST_MODIFIED_TIMESTAMP = "image_last_modified_timestamp"
 IMAGE_AGE_RETRIEVAL_ERROR = "image_age_retrieval_error"
 IMAGE_MISSING = "image_missing"
@@ -33,7 +31,7 @@ def iter_chunked(function, chunksize=500, **kwargs):
 
 
 @ttl_cache(maxsize=1, ttl=3600)
-def get_image_metrics(client):
+def get_image_metrics(client, registry):
     """Iterate over pods and produce image timestamp metrics.
 
     Parameters
@@ -59,55 +57,64 @@ def get_image_metrics(client):
         tenant = namespace_to_tenant_mapping.get(namespace, 'system')
 
         for container in iter_containers(pod):
-            registry, image, tag, is_image_internal = \
-                get_image_details(container.image)
+            registry, project, image, tag, is_image_internal = \
+                get_image_details(container.image, registry)
 
-            last_modified_time = get_last_modified_timestamp(image, tag)
+            last_modified_time, vulnerabilities, critical, high, fixable, total = get_last_modified_timestamp(
+                project, image, tag, registry)
+
+            logging.getLogger().debug(
+                'total details are follows tenant: %s, namespace: %s, registry: %s, project: %s, image_name: %s, tag: %s and time: %s',
+                tenant, namespace, registry, project, image, tag,
+                last_modified_time)
 
             if last_modified_time is None:
                 metric_id = (IMAGE_MISSING,
-                             (tenant, namespace, registry, image, tag,
+                             (tenant, namespace, registry, project, image, tag,
+                              vulnerabilities, critical, high, fixable, total,
                               str(is_image_internal).lower()))
                 metrics[metric_id] = 1
                 continue
             elif last_modified_time == 0:
                 metric_id = (IMAGE_AGE_RETRIEVAL_ERROR,
-                             (tenant, namespace, registry, image, tag,
+                             (tenant, namespace, registry, project, image, tag,
+                              vulnerabilities, critical, high, fixable, total,
                               str(is_image_internal).lower()))
                 metrics[metric_id] = 1
                 continue
 
             metric_id = (IMAGE_LAST_MODIFIED_TIMESTAMP,
-                         (tenant, namespace, registry, image, tag,
+                         (tenant, namespace, registry, project, image, tag,
+                          vulnerabilities, critical, high, fixable, total,
                           str(is_image_internal).lower()))
             metrics[metric_id] = last_modified_time
 
-    logging.getLogger().info(
-        "Preparing image metrics took %s", time.time() - start_time)
+    logging.getLogger().info("Preparing image metrics took %s",
+                             time.time() - start_time)
 
     return metrics
 
 
 @ttl_cache(ttl=900)
-def get_namespace_to_tenant_mapping(client, default_team='system'):
+def get_namespace_to_tenant_mapping(client, default_tenant='system'):
     """Retrieve namespaces and return a mapping of
-    namespace to team.
+    namespace to tenant.
 
-    If no team or label exists, uses a default team name.
+    If no tenant or label exists, uses a default tenant name.
 
     Parameters
     ----------
     client : k8s.client
         The client
 
-    default_team : str
-        The name to use if no team label exists.
+    default_tenant : str
+        The name to use if no tenant label exists.
         Defaults to 'system'
 
     Returns
     -------
     dict
-        Dictionary with namespace to team mapping
+        Dictionary with namespace to tenant mapping
     """
     namespaces = client.list_namespace()
 
@@ -115,21 +122,24 @@ def get_namespace_to_tenant_mapping(client, default_team='system'):
     for namespace in namespaces.items:
         name = namespace.metadata.name
         labels = namespace.metadata.labels
-        tenant = labels.get('team', default_team) if labels is not None \
-            else default_team
+        tenant = labels.get('tenant', default_tenant) if labels is not None \
+            else default_tenant
         namespace_to_tenant_mapping[name] = tenant
     return namespace_to_tenant_mapping
 
 
+# GET /projects/{project_name}/repositories/{repository_name}/artifacts
 @ttl_cache(ttl=3600)
-def get_last_modified_timestamp(image, tag):
-    """Retrieve image timestamp from registry and return timestamp
+def get_last_modified_timestamp(project, image, tag, registry):
+    """Retrieve image timestamp from harbor registry and return timestamp
     If an HTTP error code (e.g. 404) is returned, return None.
 
     Parameters
     ----------
+    project: str
+        The project
     image : str
-        The image
+        The repository in harbor
 
     tag : str
         The tag
@@ -142,15 +152,18 @@ def get_last_modified_timestamp(image, tag):
     """
 
     log = logging.getLogger()
+    page = 1
+    page_size = 100
 
-    manifest_url = "{}/{}/manifests/{}".format(
-        DOCKER_REGISTRY_URL, image, tag
-    )
-
-    req = urllib.request.Request(manifest_url, method='GET')
-
+    # strip project and double-quote, requests library need this
+    enc_repo = urllib.parse.quote(urllib.parse.quote('/'.join(
+        image.split('/')[1:]),
+                                                     safe=''),
+                                  safe='')
+    path = 'https://%s/api/v2.0/projects/%s/repositories/%s/artifacts/%s?page_size=%s&page=%s&with_scan_overview=true' % (
+        registry, project, enc_repo, tag, page_size, page)
     try:
-        response = urllib.request.urlopen(req)
+        response = urllib.request.urlopen(path)
     except urllib.error.HTTPError:
         return None
     else:
@@ -158,47 +171,21 @@ def get_last_modified_timestamp(image, tag):
         try:
             data = json.loads(response.read().decode("UTF-8"))
         except Exception:
-            log.warning(
-                "Failed to decode json from registry response: %s",
-                response.read()
-            )
-
-        schema_version = data.get("schemaVersion", None)
-
-        if schema_version == 1:
-            if "history" in data:
-                v1Compat = json.loads(data["history"][0]["v1Compatibility"])
-                return dateutil.parser.parse(
-                    v1Compat.get("created", "0")).timestamp()
-            else:
-                log.info(
-                    "Failed to deduce image creation data for %s:%s",
-                    image, tag
-                )
-                return 0
-
-        elif schema_version == 2:
-            media_type = data["config"]["mediaType"]
-
-            config_url = "{}/{}/blobs/{}".format(
-                DOCKER_REGISTRY_URL, image, data["config"]["digest"]
-            )
-
-            req = urllib.request.Request(
-                config_url, headers={"Accept": media_type}, method='GET')
-            try:
-                response = urllib.request.urlopen(req)
-            except urllib.error.HTTPError:
-                return 0
-
-            config = json.load(response)
-            return dateutil.parser.parse(
-                config.get("created", "0")).timestamp()
-
+            log.warning("Failed to decode json from registry response: %s",
+                        response.read())
+        scan_overview = data['scan_overview'][
+            'application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0']
+        vulnerabilities = scan_overview['severity']
+        if vulnerabilities != "Unknown":
+            scan_summary = scan_overview['summary'].get('summary')
+            critical = scan_summary.get('Critical')
+            high = scan_summary.get('High')
+            total = scan_overview['summary'].get('total')
+            fixable = scan_overview['summary'].get('fixable')
         else:
-            log.info("Unknown schema version %s for %s:%s",
-                     schema_version, image, tag)
-            return 0
+            critical = high = fixable = total = None
+        return dateutil.parser.parse(data['extra_attrs']['created']).timestamp(
+        ), vulnerabilities, critical, high, fixable, total
 
 
 def iter_containers(pod):
@@ -212,7 +199,7 @@ def iter_containers(pod):
     yield from pod.spec.containers
 
 
-def get_image_details(image_path):
+def get_image_details(image_path, registry):
     """Parse image path and split it into its components
 
     Parameters
@@ -225,6 +212,8 @@ def get_image_details(image_path):
     str
         Registry name
     str
+        Project name
+    str
         Image name
     str
         Image tag
@@ -232,7 +221,6 @@ def get_image_details(image_path):
         Flag indicating if image is internal
     """
 
-    registry = DOCKER_REGISTRY_HOST
     if "/" in image_path and "." in image_path.split("/")[0]:
         # registry probably a hostname, strip registry from image path
         registry, image_path = image_path.split("/", 1)
@@ -240,6 +228,8 @@ def get_image_details(image_path):
     # take care of situations like haproxy:latest
     if (len(image_path.split('/')) == 1):
         image_path = 'library/' + image_path
+
+    project = image_path.split('/', 1)[0]
 
     if "@" in image_path:
         # digest format image@checksum:sum
@@ -253,4 +243,4 @@ def get_image_details(image_path):
 
     is_image_internal = image_path.split('/')[0].lower() == 'internal'
 
-    return registry, image, tag, is_image_internal
+    return registry, project, image, tag, is_image_internal
